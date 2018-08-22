b1e4dd80f26fd466d9f931e48f0c92e31579f19f
massa <massafranck@gmail.com>
2018-08-22 21:55:24 +0200

    corrigé

--- user.php
+++ user.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <title>Exercice4</title>
-</head>
-<body>
-  <p>
-    <!--Récupération des données du formulaire-->
-    Bonjour : <?php echo $_POST['firstName'] . ' ' . $_POST['lastName']; ?>
-  </p>
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice4</title>
+    </head>
+    <body>
+         <?php
+        if (!empty($_POST['firstName']) && !empty($_POST['lastName'])) {
+            //Récupération des données du formulaire
+            echo 'Bonjour ' . $_POST['firstName'] . ' ' . $_POST['lastName'];
+        } else {
+            echo 'Champs vides';
+        }
+        ?>
+    </body>
 </html>
